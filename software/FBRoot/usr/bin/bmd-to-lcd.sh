9cf663fced51f3591e2a916dff5d66766cd8f986
Erik Zandvliet <erik.zandvliet@ddvtech.com>
2016-08-15 21:26:26 +0200

    Commit for streaming the bmd device to display

---
+++ software/FBRoot/usr/bin/bmd-to-lcd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#uncomment this one for HDMI
+./bmd-streamer -f /usr/lib/firmware -B -S hdmi | mplayer -fs -quiet -hardframedrop -screenw 320 -screenh 240 -ao none -vo directfb -
+
+#uncomment this one for SDI
+#./bmd-streamer -B -S sdi | mplayer -fs -quiet -hardframedrop -screenw 320 -screenh 240 -ao none -vo directfb -
+
