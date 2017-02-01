e54c7f657e89a3e0ac584f70c983dc78e775477b
Vasil Kolev <vasil@ludost.net>
2017-02-01 20:18:26 +0200

    monitoring script

---
+++ software/monitor/mon.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+aw1120_cam='udp://227.0.0.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+aw1121_cam='udp://227.0.1.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+aw1124_cam='udp://227.0.2.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+aw1125_cam='udp://227.0.3.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+aw1126_cam='udp://227.0.4.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+h1301_cam='udp://227.1.0.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+h1302_cam='udp://227.1.1.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+h1308_cam='udp://227.1.2.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+h1309_cam='udp://227.1.3.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+h2213_cam='udp://227.1.4.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+h2214_cam='udp://227.1.5.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+h2215_cam='udp://227.1.6.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+janson_cam='udp://227.2.0.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+k1105_cam='udp://227.3.0.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+k3201_cam='udp://227.3.1.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+k3401_cam='udp://227.3.2.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+k4201_cam='udp://227.3.3.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+k4401_cam='udp://227.3.4.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+k4601_cam='udp://227.3.5.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+ua2114_cam='udp://227.4.0.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+ua2220_cam='udp://227.4.1.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+ub2252a_cam='udp://227.4.2.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+ud2120_cam='udp://227.4.3.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+ud2218a_cam='udp://227.4.4.2:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+
+aw1120_slides='udp://227.0.0.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+aw1121_slides='udp://227.0.1.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+aw1124_slides='udp://227.0.2.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+aw1125_slides='udp://227.0.3.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+aw1126_slides='udp://227.0.4.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+h1301_slides='udp://227.1.0.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+h1302_slides='udp://227.1.1.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+h1308_slides='udp://227.1.2.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+h1309_slides='udp://227.1.3.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+h2213_slides='udp://227.1.4.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+h2214_slides='udp://227.1.5.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+h2215_slides='udp://227.1.6.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+janson_slides='udp://227.2.0.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+k1105_slides='udp://227.3.0.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+k3201_slides='udp://227.3.1.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+k3401_slides='udp://227.3.2.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+k4201_slides='udp://227.3.3.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+k4401_slides='udp://227.3.4.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+k4601_slides='udp://227.3.5.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+ua2114_slides='udp://227.4.0.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+ua2220_slides='udp://227.4.1.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+ub2252a_slides='udp://227.4.2.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+ud2120_slides='udp://227.4.3.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+ud2218a_slides='udp://227.4.4.1:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+
+aw1120_vocto='udp://227.0.0.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+aw1121_vocto='udp://227.0.1.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+aw1124_vocto='udp://227.0.2.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+aw1125_vocto='udp://227.0.3.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+aw1126_vocto='udp://227.0.4.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+h1301_vocto='udp://227.1.0.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+h1302_vocto='udp://227.1.1.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+h1308_vocto='udp://227.1.2.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+h1309_vocto='udp://227.1.3.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+h2213_vocto='udp://227.1.4.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+h2214_vocto='udp://227.1.5.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+h2215_vocto='udp://227.1.6.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+janson_vocto='udp://227.2.0.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+k1105_vocto='udp://227.3.0.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+k3201_vocto='udp://227.3.1.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+k3401_vocto='udp://227.3.2.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+k4201_vocto='udp://227.3.3.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+k4401_vocto='udp://227.3.4.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+k4601_vocto='udp://227.3.5.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+ua2114_vocto='udp://227.4.0.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+ua2220_vocto='udp://227.4.1.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+ub2252a_vocto='udp://227.4.2.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+ud2120_vocto='udp://227.4.3.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+ud2218a_vocto='udp://227.4.4.3:9000/?timeout=10000000&buffer_size=81921024&fifo_size=178481'
+
+aw1120_backend='rtmp://streambackend0.video.fosdem.org:1935/stream/aw1120'
+aw1121_backend='rtmp://streambackend0.video.fosdem.org:1935/stream/aw1121'
+aw1124_backend='rtmp://streambackend0.video.fosdem.org:1935/stream/aw1124'
+aw1125_backend='rtmp://streambackend0.video.fosdem.org:1935/stream/aw1125'
+aw1126_backend='rtmp://streambackend0.video.fosdem.org:1935/stream/aw1126'
+h1301_backend='rtmp://streambackend0.video.fosdem.org:1935/stream/h1301'
+h1302_backend='rtmp://streambackend1.video.fosdem.org:1935/stream/h1302'
+h1308_backend='rtmp://streambackend1.video.fosdem.org:1935/stream/h1308'
+h1309_backend='rtmp://streambackend1.video.fosdem.org:1935/stream/h1309'
+h2213_backend='rtmp://streambackend1.video.fosdem.org:1935/stream/h2213'
+h2214_backend='rtmp://streambackend1.video.fosdem.org:1935/stream/h2214'
+h2215_backend='rtmp://streambackend1.video.fosdem.org:1935/stream/h2215'
+janson_backend='rtmp://streambackend2.video.fosdem.org:1935/stream/janson'
+k1105_backend='rtmp://streambackend2.video.fosdem.org:1935/stream/k1105'
+k3201_backend='rtmp://streambackend2.video.fosdem.org:1935/stream/k3201'
+k3401_backend='rtmp://streambackend2.video.fosdem.org:1935/stream/k3401'
+k4201_backend='rtmp://streambackend2.video.fosdem.org:1935/stream/k4201'
+k4401_backend='rtmp://streambackend2.video.fosdem.org:1935/stream/k4401'
+k4601_backend='rtmp://streambackend3.video.fosdem.org:1935/stream/k4601'
+ua2114_backend='rtmp://streambackend3.video.fosdem.org:1935/stream/ua2114'
+ua2220_backend='rtmp://streambackend3.video.fosdem.org:1935/stream/ua2220'
+ub2252a_backend='rtmp://streambackend3.video.fosdem.org:1935/stream/ub2252a'
+ud2120_backend='rtmp://streambackend3.video.fosdem.org:1935/stream/ud2120'
+ud2218a_backend='rtmp://streambackend3.video.fosdem.org:1935/stream/ud2218a'
+
+
+if [ -z "$2" ]; then
+	echo "Usage: $0 room type"
+	echo "room is in (`set | grep -a _cam|cut -d _ -f 1|xargs echo`)"
+	echo "type is in (cam, slides, vocto, backend)"
+	exit 3
+fi
+
+if [ "$2" != cam ] && [ "$2" != slides ] && [ "$2" != vocto ] && [ "$2" != backend ]; then
+	echo "Unknown type $2, use cam, slides vocto or backend"
+	exit 3
+fi
+
+if ! [ -x `which mpv` ]; then
+	echo "Please install mpv"
+	exit 4
+fi
+if  mpv --list-options|grep -w lavfi-complex >/dev/null 2>/dev/null ; then
+	mpv="mpv --demuxer-lavf-analyzeduration=30  --lavfi-complex='[aid1] asplit [t1] [ao] ; [t1] showvolume=w=1000:h=100 [t2] ; [vid1]  [t2]  overlay  [vo]'"
+else
+	mpv="mpv --demuxer-lavf-analyzeduration=30"
+fi
+stream=$1_$2
+
+if [ -z "${!stream}" ]; then
+	echo ${stream} not found
+	exit 3
+fi
+echo $stream $mpv ${!stream}
