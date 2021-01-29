705fc7c4d4504d9be33027dd9e0abbcc663b00f5
Mark Van den Borre <mark+github@markvdb.be>
2021-01-29 23:19:15 +0100

    Update durations.sh

--- software/2021/durations.sh
+++ software/2021/durations.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+# Outputs the durations of the mp4 video files in the current directory.
+# Used to check the files dumped to https://validate.video.fosdem.org .
 
 rm -rf durations.txt
 for f in $(ls -1 *.mp4);do
