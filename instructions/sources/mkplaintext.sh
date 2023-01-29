dc63f2b3119a9d320948c05f4c894d5493e3b5f7
Vasil Kolev <vasil@ludost.net>
2023-01-29 14:59:14 +0200

    devroom manual: scripts update, rooms

--- instructions/sources/mkplaintext.sh
+++ instructions/sources/mkplaintext.sh
@@ -4,12 +4,12 @@ cd `dirname "$0"`
 
 for d in 1 2; do
 	for r in `cat rooms`; do
-		p=`apg -m8 -n1 -E ':$%><"'`
+		p=`pwgen -B 8 1`
 		echo "${d}-${r}:${p}"
 	done
 done
 
 for i in `seq 0 9`; do
-	p=`apg -m8 -n1 -E ':$%><"'`
+	p=`pwgen -B 8 1`
 	echo "voc${i}:${p}"
 done
--- instructions/sources/pregen.sh
+++ instructions/sources/pregen.sh
@@ -9,6 +9,6 @@ for day in 1 2; do
 		username=$day-$room
 		password=`grep ^$username plaintext | cut -d: -f 2`
 		sed "s/ROOM/$room/g;s/DAY/$day/;s/USERNAME/$username/g;s/PASSWORD/$password/g" FOSDEM_Devroom_manual.md | \
-		pandoc -f markdown -t latex --template tmpl.my --variable papersize=a4paper  --variable classoption=twocolumn  --variable columnsep=1in -V geometry:margin=2cm - -o $OUTDIR/$day-$room.pdf
+		pandoc -f markdown -t latex --variable papersize=a4paper  --variable classoption=twocolumn  --variable columnsep=1in -V geometry:margin=2cm - -o $OUTDIR/$day-$room.pdf
 	done
 done
