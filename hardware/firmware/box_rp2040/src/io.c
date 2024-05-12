87d24f362e1a761752a0682e030199e453c8f12c
dexterlb <dexterlb@qtrp.org>
2024-05-12 23:35:31 +0300

    box_firmware: USB io

--- hardware/firmware/box_rp2040/src/io.c
+++ hardware/firmware/box_rp2040/src/io.c
@@ -10,16 +10,28 @@ void io_init(void) {
 
 }
 
-void io_task(void) {
+void io_handle_char(char chr) {
+    char buf[15] = "you said: ";
+    buf[11] = chr;
+    buf[12] = '\r';
+    buf[13] = '\n';
+    buf[14] = '\0';
+    tud_cdc_write(buf, 14);
+    tud_cdc_write_flush();
+}
+
+void io_usb_cdc_task() {
     if (!tud_cdc_connected() || !tud_cdc_available()) {
         return;
     }
 
-    static uint32_t last_print = 0;
-    uint32_t now = time_us_32();
-    if (now - last_print >= 1500 * 1000) {
-        tud_cdc_write_char('b');
-        tud_cdc_write_flush();
-        last_print = now;
+    int32_t chr;
+    while ( (chr = tud_cdc_read_char()) >= 0 ) {
+        io_handle_char((char)chr);
     }
 }
+
+void io_task(void) {
+    io_usb_cdc_task();
+}
+
