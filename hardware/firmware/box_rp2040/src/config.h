28183ad9c10eefad698b4e549c70334ba71bca54
dexterlb <dexterlb@qtrp.org>
2024-06-01 22:43:26 +0300

    box_firmware: disable hardware uart by default

--- hardware/firmware/box_rp2040/src/config.h
+++ hardware/firmware/box_rp2040/src/config.h
@@ -6,8 +6,8 @@
 #define USB_CDC_WRITE_TIMEOUT_US    500000
 
 // ********* UART ***********
-// set to NULL to disable UART
-#define UART_INSTANCE               uart0
+// set to uart0, etc to enable or to NULL to disable UART
+#define UART_INSTANCE               NULL
 #define UART_BAUD                   115200
 #define UART_PIN_TX                 0
 #define UART_PIN_RX                 1
@@ -25,7 +25,7 @@
 #define DISPLAY_WIDTH               320
 #define DISPLAY_HEIGHT              240
 #define DISPLAY_ROTATION            ST7789_LANDSCAPE
-#define DISPLAY_TEXT_COLS           30
+#define DISPLAY_TEXT_COLS           42
 #define DISPLAY_TEXT_LINES          10
 
 #define COLOUR_SCHEME_GRUVBOX
