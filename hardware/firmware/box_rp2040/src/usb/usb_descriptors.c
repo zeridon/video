a5848e1a96b4c48894ff6b3cd1e05bd74467c2a0
dexterlb <dexterlb@qtrp.org>
2024-06-02 17:29:33 +0300

    box_firmware: change vid to something more suitable

--- hardware/firmware/box_rp2040/src/usb/usb_descriptors.c
+++ hardware/firmware/box_rp2040/src/usb/usb_descriptors.c
@@ -7,7 +7,7 @@
  *   [MSB]       MIDI | HID | MSC | CDC          [LSB]
  */
 #define _PID_MAP(itf, n)  ( (CFG_TUD_##itf) << (n) )
-#define USB_VID           0xDEAD
+#define USB_VID           0xF05D
 #define USB_PID           (0x4000 | _PID_MAP(CDC, 0) | _PID_MAP(MSC, 1) | _PID_MAP(HID, 2) | \
     _PID_MAP(MIDI, 3) | _PID_MAP(VENDOR, 4) )
 
