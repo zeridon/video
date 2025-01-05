93bb495212a5182ceee7173441801c3214a303d3
Albert Stefanov <aastefanov@outlook.com>
2025-01-05 17:39:21 +0200

    audio_board: update development flash.sh

--- hardware/firmware/audio_board/flash.sh
+++ hardware/firmware/audio_board/flash.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
 
-teensy_loader_cli --mcu TEENSY41 -v -w cmake-build-debug/teensy_audio.hex
+set -euo pipefail
+
+tty=/dev/tty_fosdem_audio_ctl
+build_file=teensy_audio.hex
+
+cdir="$(dirname "$(readlink -f "${0}")")"
+if [[ $# -ge 1 ]]; then
+    build_dir="$(readlink -f "${1}")"
+else
+    build_dir="${cdir}"/build
+fi
+
+if [[ -e "${tty}" ]]; then
+    stty -F "${tty}" 134
+    sleep 0.5
+fi
+
+for i in {1..3}; do
+    if teensy_loader_cli --mcu TEENSY41 -v -w "$build_dir/$build_file"; then
+        exit 0
+    fi
+done
