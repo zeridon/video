6ce8f553dc17622556e616468405d2643573a7a7
Martijn Braam <martijn@brixit.nl>
2026-01-30 19:33:46 +0000

    This changes nothing

--- hardware/firmware/audio_board/src/main.cpp
+++ hardware/firmware/audio_board/src/main.cpp
@@ -360,16 +360,14 @@ void loop() {
     int size;
     OSCMessage msg;
     if (slip.available()) {
-        while (!slip.endofPacket()) {
-            if ((size = slip.available()) > 0) {
-                while (size) {
+        while (!slip.endofPacket() && slip.available()) {
+                while (1) {
 		    int c = slip.read();
 		    if (c >= 0 ) {
                         msg.fill(c);
-			size--;
+			break;
 		    }
 		}
-            }
         }
         if (!msg.hasError()) {
             onPacketReceived(msg);
