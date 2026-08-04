9c5820c2ab4bae7b5c9c7c7fe658f7f8724c28b8
dexterlb <dexterlb@qtrp.org>
2026-08-04 21:00:28 +0300

    more elaborate reboot

--- hardware/firmware/audio_board/src/cli/commands.cpp
+++ hardware/firmware/audio_board/src/cli/commands.cpp
@@ -374,6 +374,9 @@ const Cli::CmdDescr Cli::cmds[Cli::num_cmds + 1] = {
 			audio_reset_default_state();
 			audio_eeprom_save_all();
 			cli->report_ok();
+			delay(50);
+			USB1_USBCMD = 0;        // disconnect from usb
+			delay(50);
             SCB_AIRCR = 0x05FA0004; // reboot
 		},
 	},
